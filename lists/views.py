a830b1eed004899bddbfe2e95c43c246b320cfc7
yoon <fromleaf@gmail.com>
2016-11-29 12:26:28 +0900

    recommit

---
+++ lists/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect, render
+from .models import Item
+
+
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
